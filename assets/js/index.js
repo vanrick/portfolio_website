b572bfc45c9a8649ba20a1006ec3491bad9ec56f
Rick Van <x.rckvan@gmail.com>
2018-06-26 12:35:33 -0600

    queries

--- assets/js/index.js
+++ assets/js/index.js
@@ -71,4 +71,32 @@ $(document).ready(function() {
             }
         });
 
+    // animations on scroll
+
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn')
+    }, {
+        offset: '50%'
+    })
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated rubberBand')
+    }, {
+        offset: '50%'
+    })
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeInUp')
+    }, {
+        offset: '50%'
+    })
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated fadeIn')
+    }, {
+        offset: '50%'
+    })
+    $('.js--wp-5').waypoint(function(direction) {
+        $('.js--wp-5').addClass('animated pulse')
+    }, {
+        offset: '50%'
+    })
+
 })
